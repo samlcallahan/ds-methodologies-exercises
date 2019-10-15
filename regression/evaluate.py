cfb8b1fb74ef2fd5122a0482a03442be841d6666
Sam Callahan <callahan.l.sam@gmail.com>
2019-10-15 08:58:47 -0500

    added main function

---
+++ regression/evaluate.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from scipy import stats
+from statsmodels.formula.api import ols
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+from sklearn.feature_selection import f_regression 
+from math import sqrt
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
