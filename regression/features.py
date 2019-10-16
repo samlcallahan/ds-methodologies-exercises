64d817886e159a2ebfcf42d8a64962bba77bbec3
Sam Callahan <callahan.l.sam@gmail.com>
2019-10-16 15:43:29 -0500

    Continued working on feature engineering

--- regression/features.py
+++ regression/features.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from scipy import stats
-from statsmodels.formula.api import ols
-from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression, LassoCV
 from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
-from sklearn.feature_selection import f_regression, SelectKBest
+from sklearn.feature_selection import f_regression, SelectKBest, RFE
 import matplotlib.pyplot as plt
 from pydataset import data
 import warnings
@@ -20,8 +20,40 @@ def select_kbest_freg_unscaled(X_train, y_train, k):
     freg_feature = X_train.loc[:,freg_support].columns.tolist()
     return freg_feature
 
-def select_kbest_freg_scaled():
+def select_kbest_freg_scaled(X_train, y_train, k):
+    x_scale = ss.standard_scaler(X_train, X_train)[1]
+    y_scale = ss.standard_scaler(y_train, y_train)[1]
+    return select_kbest_freg_scaled(x_scale, y_scale, k)
+
+def ols_backward_elimination(X_train, y_train):
+    cols = X_train.columns
+    pmax = 1
+    while pmax > .05:
+        if len(cols) == 0:
+            break
+        x = X_train[cols]
+        model = sm.OLS(y_train,x).fit()
+        pmax = max(model.pvalues)
+        pmax_id = model.pvalues.idxmax()
+        if pmax > .05:
+            cols.remove(pmax_id)
+    return pd.Series(cols)
+
+def lasso_cv_coef(X_train, y_train):
+    reg = LassoCV()
+    reg.fit(X_train, y_train)
+
     return
+
+def optimal_feature_n():
+    return n
+
+def top_n_features(n):
+    return features
+
+def recursive_feature_elimination(features):
+    n = optimal_feature_n()
+    
 
 seed = 43
 
@@ -32,4 +64,9 @@ X_train = train.drop(columns='total_charges').set_index('customer_id')
 y_train = train[['customer_id','total_charges']].set_index('customer_id')
 X_test = test.drop(columns='total_charges')
 y_test = test[['total_charges']]
+
 select_kbest_freg_unscaled(X_train, y_train, 1)
+x_scale = ss.standard_scaler(X_train, X_train)[1]
+y_scale = ss.standard_scaler(y_train, y_train)[1]
+select_kbest_freg_scaled(X_test, y_test, 1)
+ols_backward_elimination(x_scale, y_scale)
