c880ee23077a3c94fe177b77b091eb321cb91d65
Sam Callahan <callahan.l.sam@gmail.com>
2019-10-17 10:45:14 -0500

    Finished features exercise

--- regression/features.py
+++ regression/features.py
@@ -42,31 +42,52 @@ def ols_backward_elimination(X_train, y_train):
 def lasso_cv_coef(X_train, y_train):
     reg = LassoCV()
     reg.fit(X_train, y_train)
+    coef = pd.Series(reg.coef_, index=X_train.columns)
+    p = sns.barplot(x=X_train.columns, y=reg.coef_)
+    return coef, p
 
-    return
+def optimal_feature_n(X_train, y_train):
+    n_attributes = X_train.shape[1]
+    high_score=0
+    number_of_features=0           
+    
+    for n in range(n_attributes):
+        model = LinearRegression()
+        rfe = RFE(model, n + 1)
+        X_train_rfe = rfe.fit_transform(X_train, y_train)
+        model.fit(X_train_rfe, y_train)
+        score = model.score(X_train_rfe, y_train)
+        if(score > high_score):
+            high_score = score
+            number_of_features = n + 1
+    return number_of_features
 
-def optimal_feature_n():
-    return n
-
-def top_n_features(n):
+def top_n_features(X_train, y_train, n):
+    cols = X_train.columns
+    model = LinearRegression()
+    rfe = RFE(model, n)
+    X_rfe = rfe.fit_transform(X_train,y_train) 
+    model.fit(X_rfe,y_train)
+    features= list(cols[rfe.support_])
     return features
 
-def recursive_feature_elimination(features):
-    n = optimal_feature_n()
     
+if __name__ == '__main__':
+    seed = 43
 
-seed = 43
+    telco = wrangle.wrangle_telco()
 
-telco = wrangle.wrangle_telco()
+    train, test = ss.split_my_data(telco, .8, seed)
+    X_train = train.drop(columns='total_charges').set_index('customer_id')
+    y_train = train[['customer_id','total_charges']].set_index('customer_id')
+    X_test = test.drop(columns='total_charges')
+    y_test = test[['total_charges']]
 
-train, test = ss.split_my_data(telco, .8, seed)
-X_train = train.drop(columns='total_charges').set_index('customer_id')
-y_train = train[['customer_id','total_charges']].set_index('customer_id')
-X_test = test.drop(columns='total_charges')
-y_test = test[['total_charges']]
-
-select_kbest_freg_unscaled(X_train, y_train, 1)
-x_scale = ss.standard_scaler(X_train, X_train)[1]
-y_scale = ss.standard_scaler(y_train, y_train)[1]
-select_kbest_freg_scaled(X_test, y_test, 1)
-ols_backward_elimination(x_scale, y_scale)
+    select_kbest_freg_unscaled(X_train, y_train, 1)
+    x_scale = ss.standard_scaler(X_train, X_train)[1]
+    y_scale = ss.standard_scaler(y_train, y_train)[1]
+    select_kbest_freg_scaled(X_test, y_test, 1)
+    ols_backward_elimination(x_scale, y_scale)
+    lasso_cv_coef(x_scale, y_train)
+    n = optimal_feature_n(X_train, y_train)
+    top_n_features(X_train, y_train, n)
