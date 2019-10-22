2aef8264ed55df6deba7e9b181adfcffa75281c7
Sam Callahan <callahan.l.sam@gmail.com>
2019-10-22 12:12:52 -0500

    Added semicolons to my sql queries

--- classification/acquire.py
+++ classification/acquire.py
@@ -7,13 +7,13 @@ def get_db_url(username, hostname, password, db_name):
 
 def get_titanic_data():
     db_name = 'titanic_db'
-    query = 'select * from passengers'
+    query = 'select * from passengers;'
     url = get_db_url(user, host, password, db_name)
     return pd.read_sql(query, url)
 
 def get_iris_data():
     query = ''' select * from measurements
-                left join using(species_id)
+                left join using(species_id);
                 '''
     db_name = 'iris_db'
     url = get_db_url(user, host, password, db_name)
