6eeba569c8e377f51ccb76f58f5900c0ae3635bd
Sam Callahan <callahan.l.sam@gmail.com>
2019-10-24 10:50:43 -0500

    Imputed mode to fill nans in embark_town instead of filling with 'other'

--- classification/prepare.py
+++ classification/prepare.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.impute import SimpleImputer
 import acquire
 import warnings
 warnings.filterwarnings("ignore")
@@ -18,13 +19,15 @@ def prep_iris(iris):
     return iris, encoder
 
 def prep_titanic(titanic):
-    titanic.embark_town.fillna('Other', inplace=True)
+    titanic.fillna(np.nan, inplace=True)
     titanic.embarked.fillna('Other', inplace=True)
     titanic.drop(columns='deck', inplace=True)
     encoder = LabelEncoder()
     encoder.fit(titanic.embarked)
     titanic.embarked = encoder.transform(titanic.embarked)
     titanic_train, titanic_test = train_test_split(titanic, random_state=seed, stratify=titanic[['survived']])
+    imputer = SimpleImputer(strategy='most_frequent')
+    imputer.fit_transform(titanic_train[['embark_town']])
     scaler = MinMaxScaler()
     titanic_train[['fare', 'age']] = scaler.fit_transform(titanic_train[['fare', 'age']])
     return titanic_train, titanic_test, scaler
